0a42578546558d7b568a1ce138d9ae9f36968bcd
carloosamarante <amarante.carlos@hotmail.com>
2019-09-05 22:11:53 -0300

    Create spinner logic

--- js/home.js
+++ js/home.js
@@ -1,14 +1,18 @@
-var i = 0,
+var count = 0,
     txt = 'Bem vindo ao Simulador Genesis!',
     speed = 130;
 
 window.onload = function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("title-presentation").innerHTML += txt.charAt(i);
-    i++;
+  if (count < txt.length) {
+    document.getElementById("title-presentation").innerHTML += txt.charAt(count);
+    count++;
     setTimeout(typeWriter, speed);
   } else {
         document.getElementById("button-home").classList.remove("hide-button-effect");
         document.getElementById("button-home").classList.add("button-effect");
+  }
+
+  document.getElementById("button-home").onclick = function loading() {
+    document.getElementById("loading").classList.remove("hide");
   }
 }
