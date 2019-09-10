fd88e858eee349bfb07f878fb6b0bdfc336432b4
carloosamarante <amarante.carlos@hotmail.com>
2019-09-10 00:58:15 -0300

    Align title and put plus icon in layout

--- js/modules.js
+++ js/modules.js
@@ -1,20 +1,13 @@
-var count = 0,
-    txt = 'M\u00f3dulos',
-    speed = 130;
-
 window.onload = function typeWriter() {
-  var moduleIcon = document.getElementById("module-icon");
-  var moduleBox = document.getElementsByClassName("modules-box");
-  
-  if (count < txt.length) {
-    document.getElementById("module-title").innerHTML += txt.charAt(count);
-    count++;
-    setTimeout(typeWriter, speed);
-  } else {
-    moduleIcon.classList.remove("hide-button-effect");
-    moduleIcon.classList.remove("button-effect");
-    showModules();
-  }
+  var moduleIcon = document.getElementById("module-icon"),
+      moduleBox = document.getElementsByClassName("modules-box"),
+      plusIcon = document.getElementById("plus-icon");
+
+  moduleIcon.classList.remove("hide-button-effect");
+  moduleIcon.classList.add("button-effect");
+  plusIcon.classList.remove("hide-button-effect");
+  plusIcon.classList.add("button-effect");
+  showModules();
 
   function showModules() {
     for (var i = 0; i < moduleBox.length; ++i) {
