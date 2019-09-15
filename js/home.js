c9b254126eb858ea3caf3ef456e192b1043058df
carloosamarante <amarante.carlos@hotmail.com>
2019-09-15 19:20:56 -0300

    Fix load js

--- js/home.js
+++ js/home.js
@@ -3,21 +3,23 @@ var count = 0,
     speed = 130;
 
 window.onload = function typeWriter() {
-  connectButton = document.getElementById("button-home");
+  if (window.location.pathname.match('home.html') != null) {
+    connectButton = document.getElementById("button-home");
 
-  if (count < txt.length) {
-    document.getElementById("title-presentation").innerHTML += txt.charAt(count);
-    count++;
-    setTimeout(typeWriter, speed);
-  } else {
-        connectButton.classList.remove("hide-button-effect");
-        connectButton.classList.add("button-effect");
-  }
-  
-  spinnerLoading = document.getElementById("loading");
-  connectButton.onclick = function loading() {
-    connectButton.classList.add("hide");
-    spinnerLoading.classList.remove("hide-button-effect");
-    spinnerLoading.classList.add("button-effect");
+    if (count < txt.length) {
+      document.getElementById("title-presentation").innerHTML += txt.charAt(count);
+      count++;
+      setTimeout(typeWriter, speed);
+    } else {
+          connectButton.classList.remove("hide-button-effect");
+          connectButton.classList.add("button-effect");
+    }
+    
+    spinnerLoading = document.getElementById("loading");
+    connectButton.onclick = function loading() {
+      connectButton.classList.add("hide");
+      spinnerLoading.classList.remove("hide-button-effect");
+      spinnerLoading.classList.add("button-effect");
+    }
   }
 }
--- js/modules.js
+++ js/modules.js
@@ -1,5 +1,6 @@
-window.onload = function typeWriter() {
-  var moduleIcon = document.getElementById("module-icon"),
+document.addEventListener("DOMContentLoaded", function(event) {
+  if (window.location.pathname.match('modules.html') != null) {
+    var moduleIcon = document.getElementById("module-icon"),
       moduleBox = document.getElementsByClassName("modules-box"),
       plusIcon = document.getElementById("plus-icon"),
       timeModule = document.getElementById("time-module"),
@@ -7,46 +8,47 @@ window.onload = function typeWriter() {
       timePeriodButton = document.getElementById("period-time-button");
       timeClockButton = document.getElementById("time-clock-button");
 
-  moduleIcon.classList.remove("hide-button-effect");
-  moduleIcon.classList.add("button-effect");
-  plusIcon.classList.remove("hide-button-effect");
-  plusIcon.classList.add("button-effect");
-  showModules();
+    moduleIcon.classList.remove("hide-button-effect");
+    moduleIcon.classList.add("button-effect");
+    plusIcon.classList.remove("hide-button-effect");
+    plusIcon.classList.add("button-effect");
+    showModules();
 
-  function showModules() {
-    for (var i = 0; i < moduleBox.length; ++i) {
-        moduleBox[i].classList.remove("hide-button-effect");
-        moduleBox[i].classList.add("button-effect");
+    function showModules() {
+      for (var i = 0; i < moduleBox.length; ++i) {
+          moduleBox[i].classList.remove("hide-button-effect");
+          moduleBox[i].classList.add("button-effect");
+      }
+    }
+
+    timeModule.onclick = function showTime() {
+      timeModal.classList.remove("hide-button-effect");
+      timeModal.classList.add("button-effect");
+    }
+
+    window.onclick = function(event) {
+      if (event.target == timeModal) {
+        timeModal.classList.remove("button-effect");
+        timeModal.classList.add("hide-button-effect");
+      }
+    }
+
+    timeClockButton.onclick = function(){
+      changePeriod('clock');
+    };
+
+    timePeriodButton.onclick = function(){
+      changePeriod('period');
+    };
+
+    function changePeriod(typeButton) {
+      if (typeButton == 'clock') {
+        timec = document.getElementById("timec").value;
+        document.getElementById("hour-clock").innerHTML = timec
+      } else {
+        timep = document.getElementById("timep").value;
+        document.getElementById("hour-period").innerHTML = timep
+      }
     }
   }
-
-  timeModule.onclick = function showTime() {
-    timeModal.classList.remove("hide-button-effect");
-    timeModal.classList.add("button-effect");
-  }
-
-  window.onclick = function(event) {
-    if (event.target == timeModal) {
-      timeModal.classList.remove("button-effect");
-      timeModal.classList.add("hide-button-effect");
-    }
-  }
-
-  timeClockButton.onclick = function(){
-    changePeriod('clock');
-  };
-
-  timePeriodButton.onclick = function(){
-    changePeriod('period');
-  };
-
-  function changePeriod(typeButton) {
-    if (typeButton == 'clock') {
-      timec = document.getElementById("timec").value;
-      document.getElementById("hour-clock").innerHTML = timec
-    } else {
-      timep = document.getElementById("timep").value;
-      document.getElementById("hour-period").innerHTML = timep
-    }
-  }
-}
+})
