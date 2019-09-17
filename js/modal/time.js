9a7d1e11c47def16feea2678d0267d7ee99f3ebf
carloosamarante <amarante.carlos@hotmail.com>
2019-09-17 19:08:29 -0300

    Fix js from modals

--- js/modal/time.js
+++ js/modal/time.js
@@ -1,65 +1,63 @@
-document.addEventListener("DOMContentLoaded", function (eventTime) {
-  if (window.location.pathname.match('modules.html') != null) {
-    var timeModule = document.getElementById("time-module"),
-      timeModal = document.getElementById("time-modal"),
-      timePeriodButton = document.getElementById("period-time-button"),
-      timeClockButton = document.getElementById("time-clock-button"),
-      TimeClockHttp = new XMLHttpRequest(),
-      TimePeriodHttp = new XMLHttpRequest(),
-      url = 'http://localhost:5051/healthcheck';
+if (window.location.pathname.match('modules.html') != null) {
+  var timeModule = document.getElementById("time-module"),
+    timeModal = document.getElementById("time-modal"),
+    timePeriodButton = document.getElementById("period-time-button"),
+    timeClockButton = document.getElementById("time-clock-button"),
+    TimeClockHttp = new XMLHttpRequest(),
+    TimePeriodHttp = new XMLHttpRequest(),
+    url = 'http://localhost:5051/healthcheck';
 
-    timeModule.onclick = function showTime() {
-      timeModal.classList.remove("hide-button-effect");
-      timeModal.classList.add("button-effect");
-    }
+  timeModule.onclick = function showTime() {
+    timeModal.classList.remove("hide-button-effect");
+    timeModal.classList.add("button-effect");
+  }
 
-    timeClockButton.onclick = function () {
-      changePeriod('clock');
-    };
+  timeClockButton.onclick = function () {
+    changePeriod('clock');
+  };
 
-    timePeriodButton.onclick = function () {
-      changePeriod('period');
-    };
+  timePeriodButton.onclick = function () {
+    changePeriod('period');
+  };
 
-    function changePeriod(typeButton) {
-      if (typeButton == 'clock') {
-        var timec = document.getElementById("timec").value;
+  function changePeriod(typeButton) {
+    if (typeButton == 'clock') {
+      var timec = document.getElementById("timec").value;
 
-        TimeClockHttp.open('POST', url);
-        TimeClockHttp.onreadystatechange = function () {
-          if (TimeClockHttp.readyState != 4) {
-            timeClockButton.innerHTML = "Alterando...";
-            timeClockButton.classList.add("disabled");
-          }
-          if (TimeClockHttp.readyState == 4 && TimeClockHttp.status == 200) {
-            setTimeout(function () {
-              document.getElementById("hour-clock").innerHTML = timec;
-              timeClockButton.innerHTML = "Alterar";
-              timeClockButton.classList.remove("disabled");
-            }, 3000);
-          }
+      TimeClockHttp.open('POST', url);
+      TimeClockHttp.onreadystatechange = function () {
+        if (TimeClockHttp.readyState != 4) {
+          timeClockButton.innerHTML = "Alterando...";
+          timeClockButton.classList.add("disabled");
         }
-        TimeClockHttp.send(timec);
-
-      } else if (typeButton == 'period') {
-        var timep = document.getElementById("timep").value;
-
-        TimePeriodHttp.open('POST', url);
-        TimePeriodHttp.onreadystatechange = function () {
-          if (TimePeriodHttp.readyState != 4) {
-            timePeriodButton.innerHTML = "Alterando...";
-            timePeriodButton.classList.add("disabled");
-          }
-          if (TimePeriodHttp.readyState == 4 && TimePeriodHttp.status == 200) {
-            setTimeout(function () {
-              document.getElementById("hour-period").innerHTML = timep;
-              timePeriodButton.innerHTML = "Alterar";
-              timePeriodButton.classList.remove("disabled");
-            }, 3000);
-          }
+        if (TimeClockHttp.readyState == 4 && TimeClockHttp.status == 200) {
+          setTimeout(function () {
+            document.getElementById("hour-clock").innerHTML = timec;
+            timeClockButton.innerHTML = "Alterar";
+            timeClockButton.classList.remove("disabled");
+          }, 3000);
         }
-        TimePeriodHttp.send(timep);
       }
+      TimeClockHttp.send(timec);
+
+    } else if (typeButton == 'period') {
+      var timep = document.getElementById("timep").value;
+
+      TimePeriodHttp.open('POST', url);
+      TimePeriodHttp.onreadystatechange = function () {
+        if (TimePeriodHttp.readyState != 4) {
+          timePeriodButton.innerHTML = "Alterando...";
+          timePeriodButton.classList.add("disabled");
+        }
+        if (TimePeriodHttp.readyState == 4 && TimePeriodHttp.status == 200) {
+          setTimeout(function () {
+            document.getElementById("hour-period").innerHTML = timep;
+            timePeriodButton.innerHTML = "Alterar";
+            timePeriodButton.classList.remove("disabled");
+          }, 3000);
+        }
+      }
+      TimePeriodHttp.send(timep);
     }
   }
-});
+}
--- js/modal/velocity.js
+++ js/modal/velocity.js
@@ -1,50 +1,48 @@
-document.addEventListener("DOMContentLoaded", function (eventVelocity) {
-  if (window.location.pathname.match('modules.html') != null) {
-    var velocityModule = document.getElementById("velocity-module"),
-      velocityModal = document.getElementById("velocity-modal"),
-      velocityButton = document.getElementById("odometer-button"),
-      velocityHttp = new XMLHttpRequest(),
-      url = 'http://localhost:5051/healthcheck';
+if (window.location.pathname.match('modules.html') != null) {
+  var velocityModule = document.getElementById("velocity-module"),
+    velocityModal = document.getElementById("velocity-modal"),
+    velocityButton = document.getElementById("odometer-button"),
+    velocityHttp = new XMLHttpRequest(),
+    url = 'http://localhost:5051/healthcheck';
 
-      velocityButton.onclick = function () {
-        changeSpeed();
-      };
+    velocityButton.onclick = function () {
+      changeSpeed();
+    };
 
-    velocityModule.onclick = function () {
-      velocityModal.classList.remove("hide-button-effect");
-      velocityModal.classList.add("button-effect");
-    }
+  velocityModule.onclick = function () {
+    velocityModal.classList.remove("hide-button-effect");
+    velocityModal.classList.add("button-effect");
+  }
 
-    function changeSpeed() {
-      var speed = document.getElementById("odometer").value;
+  function changeSpeed() {
+    var speed = document.getElementById("odometer").value;
 
-      velocityHttp.open('POST', url);
-      velocityHttp.onreadystatechange = function () {
-        if (velocityHttp.readyState != 4) {
-          velocityButton.innerHTML = "Alterando...";
-          velocityButton.classList.add("disabled");
-        }
-        if (velocityHttp.readyState == 4 && velocityHttp.status == 200) {
-          setTimeout(function () {
-            document.getElementById("velocity").innerHTML = speed;
-            calcSpeedMarker(speed);
-            velocityButton.innerHTML = "Alterar";
-            velocityButton.classList.remove("disabled");
-          }, 3000);
-        }
+    velocityHttp.open('POST', url);
+    velocityHttp.onreadystatechange = function () {
+      if (velocityHttp.readyState != 4) {
+        velocityButton.innerHTML = "Alterando...";
+        velocityButton.classList.add("disabled");
       }
-        velocityHttp.send(speed);
+      if (velocityHttp.readyState == 4 && velocityHttp.status == 200) {
+        setTimeout(function () {
+          document.getElementById("velocity").innerHTML = speed;
+          calcSpeedMarker(speed);
+          velocityButton.innerHTML = "Alterar";
+          velocityButton.classList.remove("disabled");
+        }, 3000);
+      }
     }
+      velocityHttp.send(speed);
+  }
 
-    function calcSpeedMarker(speed) {
-      [].forEach.call(document.querySelectorAll('.marker-opacity'), function (el) {
-        el.classList.remove('marker-opacity');
-      });
-      marker = Math.round(speed/15);
-      for (var i = 1; i <= marker; i++) {
-        id = "marker-" + i
-        document.getElementById(id).classList.add("marker-opacity");
-     }
+  function calcSpeedMarker(speed) {
+    [].forEach.call(document.querySelectorAll('.marker-opacity'), function (el) {
+      el.classList.remove('marker-opacity');
+    });
+    marker = Math.round(speed/15);
+    for (var i = 1; i <= marker; i++) {
+      id = "marker-" + i
+      document.getElementById(id).classList.add("marker-opacity");
     }
   }
-})
+}
