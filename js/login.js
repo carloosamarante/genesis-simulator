3f7b350aa3d077cba3314d1c5f53623276431102
carloosamarante <amarante.carlos@hotmail.com>
2019-10-01 22:08:34 -0300

    Remove param from get

--- js/login.js
+++ js/login.js
@@ -33,7 +33,7 @@ window.onload = function typeWriter() {
         }
       }
       connectHttp.setRequestHeader("Content-type", "application/json");
-      connectHttp.send('conectando');
+      connectHttp.send();
     }
   }
 }
